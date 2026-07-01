36d6efd3f787c9a67aed4bb62e1e0928b97bef25
Heri Prasetyo <herpras.dev@gmail.com>
2026-07-01 08:44:16 +0000

    fix: wrap app with TenantProvider in main.tsx

--- nextrade-frontend/src/main.tsx
+++ nextrade-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./lib/AuthContext";
+import { TenantProvider } from "./store/tenantContext";
 import App from "./App";
 import "./i18n";
 import i18n from "./i18n";
@@ -12,7 +13,9 @@ i18n.changeLanguage(persistedLanguage);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <TenantProvider>
+        <App />
+      </TenantProvider>
     </AuthProvider>
   </React.StrictMode>
 );
